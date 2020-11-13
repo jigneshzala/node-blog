0ee1bf069e74e44cb72389d050280e6b49cc644c
jignesh zala <jignesh.zala@indianic.com>
2020-11-13 10:41:43 +0530

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hellow');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Lisitnig on port ${PORT}`);
+});
