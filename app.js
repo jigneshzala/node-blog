0b3783ee3d5c38f2dc92ecf5f87b146035ec02c6
jignesh zala <jigneshkumar1494@gmail.com>
2020-11-13 11:03:08 +0530

    Mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
 
+dotenv.config({
+    path: './config.env'
 
+});
+//Routes
 app.get('/', (req, res) => {
     res.send('Hellow');
 });
+app.get('/home', (req, res) => {
+    res.send('home');
+});
+app.get('/category', (req, res) => {
+    res.send('Category');
+});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB Connection successfull!'));
+
 
 const PORT = 3000;
 
